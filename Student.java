36fb5386cee210bec90bbb0d357ee6c052ec3f27
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-03 20:05:22 +0530

    Created Student.java
    Initialized a parametrized constructor

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name;
+    private long prn;
+    private String dept;
+    private String batch;
+    private double cgpa;
+
+    public Student(String name, long prn, String dept, String batch, double cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.dept = dept;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
+
