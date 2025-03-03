2cb9617687a4271dd2b32313ef92e24b7c04a6e6
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-03 20:08:22 +0530

    Added method to add new student

--- StudentOperations.java
+++ StudentOperations.java
@@ -3,3 +3,9 @@ import java.util.*;
 public class StudentOperations {
     private ArrayList<Student> students = new ArrayList<>();
 
+	// method to add new student
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student added successfully!");
+    }
+
