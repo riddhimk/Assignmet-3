1486db946dc049ad0d19c01b5288a013802118cf
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-03 20:08:46 +0530

    Added method to display students

--- StudentOperations.java
+++ StudentOperations.java
@@ -9,3 +9,14 @@ public class StudentOperations {
         System.out.println("Student added successfully!");
     }
 
+	// method to display students
+    public void displayStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students to display.");
+        } else {
+            for (Student student : students) {
+                student.display();
+            }
+        }
+    }
+
